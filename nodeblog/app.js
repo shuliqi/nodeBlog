488393e7ae6fed747feba5817491592cf0e3056b
shuliqi <shuliqi@meituan.com>
2016-05-17 20:16:19 +0800

    基础知识

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -8,34 +8,34 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express();  //生成一个express实例 app
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));//设置 views 文件夹为存放视图文件的目录, 即存放模板文件的地方,__dirname 为全局变量,存储当前正在执行的脚本所在的目录
+app.set('view engine', 'ejs');//设置视图模板引擎为 ejs
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));  //设置/public/favicon.ico为favicon图标
+app.use(logger('dev'));//加载日志中间件
+app.use(bodyParser.json());//加载解析json的中间件
+app.use(bodyParser.urlencoded({ extended: false }));//加载解析urlencoded请求体的中间件
+app.use(cookieParser());//加载解析cookie的中间件
+app.use(express.static(path.join(__dirname, 'public')));//设置public文件夹为存放静态文件的目录
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes);//路由控制器
+app.use('/users', users);//路由控制器
 
-// catch 404 and forward to error handler
+//捕获404错误，并转发到错误处理器
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+
 // error handlers
 
-// development error handler
-// will print stacktrace
+//开发环境下的错误处理器，将错误信息渲染error模版并显示到浏览器中。
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +46,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// 生产环境下的错误处理器，不会将错误信息泄露给用户。
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -57,4 +56,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = app;//导出app实例供其他模块调用
